239ffad75f7f73938967cd8dd15a651e1f71c967
Brian Ferry <bferry@redhat.com>
2022-12-15 10:52:22 -0500

    adding folding method to config

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,4 +1,6 @@
 lvim.colorscheme = "catppuccin"
 lvim.transparent_window = false;
 lvim.builtin.gitsigns.opts.current_line_blame = true
-lvim.lsp.diagnostics.virtual_text = false
+lvim.lsp.diagnostics.virtual_text = true
+vim.opt.foldmethod = "expr";
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()";
