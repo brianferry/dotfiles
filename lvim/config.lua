f285c94e7a2d54366f150ff583877dab1c0a4944
Brian Ferry <bferry@redhat.com>
2022-12-15 08:59:45 -0500

    feat: making lvim window opaque

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,4 +1,4 @@
 lvim.colorscheme = "catppuccin-mocha"
-lvim.transparent_window = true;
+lvim.transparent_window = false;
 lvim.builtin.gitsigns.opts.current_line_blame = true
 lvim.lsp.diagnostics.virtual_text = false
