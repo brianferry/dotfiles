8e36ba822892997cc7afa0f4c8db122acdae61a8
Brian Ferry <brian.z.ferry@gmail.com>
2022-12-16 05:28:09 -0500

    fix: disable virtual text

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,7 +1,7 @@
 lvim.colorscheme = "catppuccin"
 lvim.transparent_window = false;
 lvim.builtin.gitsigns.opts.current_line_blame = true
-lvim.lsp.diagnostics.virtual_text = true
+lvim.lsp.diagnostics.virtual_text = false
 vim.opt.foldmethod = "expr";
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()";
 vim.opt.foldenable = false
